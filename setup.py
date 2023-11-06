7156e2595edb317706f967cea75c0c45b86b167b
maxi <maxi.linke@fau.de>
2023-11-06 20:58:52 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name="math_quiz",
       version="0.1",
       description="A Simple Math Quiz",
-      packages= ["math_quiz","test_math_quiz"],
+      packages= ["math_quiz"],
       zip_safe=False
     )
