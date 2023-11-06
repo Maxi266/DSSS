d0b720e60f5cc209748a14fac41383c27d556760
maxi <maxi.linke@fau.de>
2023-11-06 20:53:45 +0100

    Adding pip install compatibility

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DSSS
 Homeworks for DSSS Course in WS23/24
+
+To install use pip install git https://github.com/Maxi266/DSSS
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="math_quiz",
+      version="0.1",
+      description="A Simple Math Quiz",
+      packages= ["math_quiz","test_math_quiz"],
+      zip_safe=False
+    )
